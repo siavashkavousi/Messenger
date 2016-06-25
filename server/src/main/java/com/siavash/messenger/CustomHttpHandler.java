7b3642ecd7b9b64901c7cfbca5d802d317331f34
siavash <siavash.kavousi.sk@gmail.com>
2016-06-26 00:50:57 +0430

    Separates GET and POST responses

--- server/src/main/java/com/siavash/messenger/CustomHttpHandler.java
+++ server/src/main/java/com/siavash/messenger/CustomHttpHandler.java
@@ -1,14 +1,14 @@
 package com.siavash.messenger;
 
 import com.google.gson.Gson;
-import com.siavash.messenger.data.User;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by sia on 6/25/16.
@@ -19,23 +19,73 @@ public class CustomHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String context = httpExchange.getRequestURI().toString();
-        String[] uriSegments = context.split("\\?");
-        switch (uriSegments[0]) {
-            case "/user":
-                log.info("GET request with /user url and username as parameter");
-                String username = uriSegments[1].split("=")[1];
-                log.info("requested username: " + username);
+        String path = httpExchange.getRequestURI().getPath();
+        if (httpExchange.getRequestMethod().equals("GET")) {
+            switch (path) {
+                case "/user":
+                    log.info("GET request with /user url and username as parameter");
 
-                User user = MainApp.queries.findUser(username);
+                    Map<String, String> queryMap = queryToMap(httpExchange.getRequestURI().getQuery());
+                    String username = queryMap.get("username");
+                    log.info("requested username: " + username);
+                    User user = MainApp.queries.findUser(username);
 
-                user = new User("sia", "siavash", "kavousi", "09213456");
-                String response = gson.toJson(user);
-                httpExchange.sendResponseHeaders(200, response.length());
-                OutputStream os = httpExchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-                break;
+                    user = new User("sia", "siavash", "kavousi", "09213456");
+                    String response = gson.toJson(user);
+                    httpExchange.sendResponseHeaders(200, response.length());
+                    OutputStream os = httpExchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                    break;
+            }
+        } else if (httpExchange.getRequestMethod().equals("POST")) {
+            switch (path) {
+                case "/user":
+                    log.info("POST request with /user url and user data as parameter");
+
+                    String requestBody = readFromInputStream(httpExchange.getRequestBody());
+                    User user = gson.fromJson(requestBody, User.class);
+
+                    MainApp.insertions.insertUser(
+                            user.getUserName(),
+                            user.getFirstName(),
+                            user.getLastName(),
+                            user.getPhoneNumber());
+//                    String response =
+            }
         }
+    }
+
+    private Map<String, String> queryToMap(String query) {
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String pair[] = param.split("=");
+            if (pair.length > 1) {
+                result.put(pair[0], pair[1]);
+            } else {
+                result.put(pair[0], "");
+            }
+        }
+        return result;
+    }
+
+    private String readFromInputStream(InputStream inputStream) {
+        String result = null, line;
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
