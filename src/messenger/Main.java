55cb566bb5794e2c09eeae533760f12688b773d6
siavash <siavash.kavousi.sk@gmail.com>
2016-06-25 07:54:14 +0430

    Makes separation between menu and the rest of the layout

--- src/messenger/Main.java
+++ src/messenger/Main.java
@@ -4,16 +4,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Stage primaryStage;
+    private VBox rootLayout;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("messenger.fxml"));
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("Messenger");
+
+        Parent root = FXMLLoader.load(getClass().getResource("root_layout.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+    }
+
+    public void setUpRootLayout(){
+
     }
 
 
