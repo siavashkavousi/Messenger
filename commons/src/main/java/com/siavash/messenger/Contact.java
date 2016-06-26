7919057c9b8653c79ae23c6b584ebaf68be26b0b
siavash <siavash.kavousi.sk@gmail.com>
2016-06-26 21:18:38 +0430

    Changes if statements to switch

--- commons/src/main/java/com/siavash/messenger/Contact.java
+++ commons/src/main/java/com/siavash/messenger/Contact.java
@@ -3,12 +3,17 @@ package com.siavash.messenger;
 /**
  * Created by sia on 6/26/16.
  */
-public class Contact extends User {
+public class Contact {
     private String parentUserName;
+    private String contactUserName;
+    private String firstName;
+    private String lastName;
 
-    public Contact(String parentUserName, String userName, String firstName, String lastName, String phoneNumber) {
-        super(userName, firstName, lastName, phoneNumber);
+    public Contact(String parentUserName, String contactUserName, String firstName, String lastName) {
         this.parentUserName = parentUserName;
+        this.contactUserName = contactUserName;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getParentUserName() {
@@ -19,10 +24,37 @@ public class Contact extends User {
         this.parentUserName = parentUserName;
     }
 
+    public String getContactUserName() {
+        return contactUserName;
+    }
+
+    public void setContactUserName(String contactUserName) {
+        this.contactUserName = contactUserName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     @Override
     public String toString() {
         return "Contact{" +
                 "parentUserName='" + parentUserName + '\'' +
+                ", contactUserName='" + contactUserName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
                 '}';
     }
 }
--- server/src/main/java/com/siavash/messenger/HttpPostHandler.java
+++ server/src/main/java/com/siavash/messenger/HttpPostHandler.java
@@ -21,53 +21,56 @@ public class HttpPostHandler {
     }
 
     public void handle(HttpExchange httpExchange, String path) throws IOException {
-        if (path.equals("/user")) {
-            log.info("POST request with /user url and user data as parameter");
+        switch (path) {
+            case "/user": {
+                log.info("POST request with /user url and user data as parameter");
 
-            String requestBody = Util.readFromInputStream(httpExchange.getRequestBody());
-            User user = gson.fromJson(requestBody, User.class);
+                String requestBody = Util.readFromInputStream(httpExchange.getRequestBody());
+                User user = gson.fromJson(requestBody, User.class);
 
-            MainApp.insertions.insertUser(
-                    user.getUserName(),
-                    user.getFirstName(),
-                    user.getLastName(),
-                    user.getPhoneNumber());
-            String response = Constants.HTTP_ACCEPTED;
-            httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_ACCEPTED, response.length());
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        } else if (path.equals("/msg")) {
-            log.info("POST request with /msg url and message data as parameter");
+                MainApp.insertions.insertUser(
+                        user.getUserName(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getPhoneNumber());
+                sendOkMessage(httpExchange);
+                break;
+            }
+            case "/msg": {
+                log.info("POST request with /msg url and message data as parameter");
 
-            String requestBody = Util.readFromInputStream(httpExchange.getRequestBody());
-            Message message = gson.fromJson(requestBody, Message.class);
+                String requestBody = Util.readFromInputStream(httpExchange.getRequestBody());
+                Message message = gson.fromJson(requestBody, Message.class);
 
 //            MainApp.insertions.insertMessage(
 //                    message.getSender(),
 //                    message.getContent(),
 //                    message.getReceivers());
-            String response = Constants.HTTP_ACCEPTED;
-            httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_ACCEPTED, response.length());
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        } else if (path.equals("/contact")){
-            log.info("POST request with /contact url and user data as parameter");
+                sendOkMessage(httpExchange);
+                break;
+            }
+            case "/contact": {
+                log.info("POST request with /contact url and user data as parameter");
 
-            String requestBody = Util.readFromInputStream(httpExchange.getRequestBody());
-            User user = gson.fromJson(requestBody, User.class);
+                String requestBody = Util.readFromInputStream(httpExchange.getRequestBody());
+                Contact contact = gson.fromJson(requestBody, Contact.class);
 
-            MainApp.insertions.insertContacts(
-                    user.getUserName(),
-                    user.getFirstName(),
-                    user.getLastName(),
-                    user.getPhoneNumber());
-            String response = Constants.HTTP_ACCEPTED;
-            httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_ACCEPTED, response.length());
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+                MainApp.insertions.insertContacts(
+                        contact.getParentUserName(),
+                        contact.getContactUserName(),
+                        contact.getFirstName(),
+                        contact.getLastName());
+                sendOkMessage(httpExchange);
+                break;
+            }
         }
+    }
+
+    private void sendOkMessage(HttpExchange httpExchange) throws IOException {
+        String response = Constants.HTTP_ACCEPTED;
+        httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_ACCEPTED, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
