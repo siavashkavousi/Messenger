93aa44a6b40f1a5866aae0228cc94c02c4c58f4e
siavash <siavash.kavousi.sk@gmail.com>
2016-07-01 16:11:35 +0430

    Oops!

--- client/src/main/java/com/siavash/messenger/controllers/ContactProfile.java
+++ client/src/main/java/com/siavash/messenger/controllers/ContactProfile.java
@@ -4,12 +4,11 @@ import com.siavash.messenger.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 
 /**
  * Created by sia on 7/1/16.
  */
-public class ContactProfile extends BorderPane implements ParentProvider, ModelProvider {
+public class ContactProfile implements ParentProvider, ModelProvider {
     private ScreenManager parent;
     @FXML
     private Label userName;
